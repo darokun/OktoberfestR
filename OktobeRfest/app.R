731e1514aed4dac623460c992c4019ec2ccffe6e
darokun <daloharodriguez@gmail.com>
2017-09-28 16:52:18 +0200

    create ui

--- OktobeRfest/app.R
+++ OktobeRfest/app.R
@@ -1,5 +1,7 @@
 library(shiny)
 
+data <- read.csv("dataOktoberfest.csv")
+
 ui <- fluidPage(
   
   # Application title
@@ -8,14 +10,30 @@ ui <- fluidPage(
   # Sidebar with a slider input for range of years 
   sidebarLayout(
     sidebarPanel(
-      sliderInput(
-      )
+      sliderInput(inputId = "yearInput",
+                  label = "Choose a range of years",
+                  min = 1985,
+                  max = 2016,
+                  value = c(1985, 2016),
+                  sep = ""),
+      
+      selectInput(inputId = "variableInput",
+                  label = "What do you want to visualize?",
+                  choices = c("Mean visitors per day",
+                              "Visitors per year",
+                              "Beer price",
+                              "Amount of beer sold",
+                              "Chicken price",
+                              "Amount of chicken sold"),
+                  selected = "Beer price")
     ),
   
   
     # Show a plot of the generated distribution
     mainPanel(
-      
+      plotOutput(outputId = "distPlot"),
+      br(), br(),
+      tableOutput(outputId = "summaryTable")
     )
   )
 )
