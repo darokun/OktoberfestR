33df3181bf62833f36bfc722d2d4f13bce32e1c0
darokun <daloharodriguez@gmail.com>
2017-10-04 15:08:52 +0200

    write chunk 5

--- Diamonds/app.R
+++ Diamonds/app.R
@@ -5,12 +5,11 @@ ui <- fluidPage(
                 titlePanel("Title"), 
                 sidebarLayout(
                               sidebarPanel("Sidebar Panel",
-                                            sliderInput(
-                                                        inputId="bins",
-                                                        label="Slider Label", 
-                                                        min=1,
-                                                        max=30,
-                                                        value=15
+                                            selectInput(
+                                                        inputId="list",
+                                                        label="List Label", 
+                                                        choices=c("Fair","Good","Very Good"),
+                                                        selected="Good"
                                                         )
                                             ),
                               mainPanel("Main Panel")
