65aaa0e993db77967634ea68a8437d3df6e386cc
darokun <daloharodriguez@gmail.com>
2017-10-04 16:03:42 +0200

    write chunk 8

--- Diamonds/app.R
+++ Diamonds/app.R
@@ -19,6 +19,9 @@ server <- function(input, output) {
   output$plot1 <- renderPlot({
     ggplot(data=diamonds, aes(x=price)) + geom_histogram()
   })
+  output$plot2 <- renderPlot({
+    ggplot(data=diamonds, aes(x=carat)) + geom_histogram()
+  })
 }
 
 shinyApp(ui = ui, server = server)
