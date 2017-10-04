a74c52eb5fa7a4e577df474845dc5cc95c621fc7
darokun <daloharodriguez@gmail.com>
2017-10-04 14:58:34 +0200

    write chunk 2

--- Diamonds/app.R
+++ Diamonds/app.R
@@ -1,7 +1,11 @@
 #Remember to load & define variables for the global environment, e.g. packages, objects
 library(shiny)
 
-ui <- basicPage()
+ui <- pageWithSidebar(
+                      titlePanel("Title"), 
+                      sidebarPanel("Sidebar Panel"), 
+                      mainPanel("Main Panel")
+)
 
 server <- function(input, output) {}
 
