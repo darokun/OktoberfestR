cec60f5fb9fd462fa37c0a0d1d3c9fa27f3ba823
darokun <daloharodriguez@gmail.com>
2017-10-04 16:24:03 +0200

    write chunk 11: full app

--- Diamonds/app.R
+++ Diamonds/app.R
@@ -1,32 +1,53 @@
 #Remember to load & define variables for the global environment, e.g. packages, objects
 library(shiny) 
 library(ggplot2)
+library(RColorBrewer)
 
 ui <- fluidPage(
-  titlePanel("Example Shiny App: Distribution of Diamond stock by Price Bands"), 
+  titlePanel("Example Shiny App: Tool for Exploring Diamond Data"), 
   sidebarLayout(
     
     sidebarPanel(
-      sliderInput(
-        inputId="bands",
-        label="Select No. of Price Bands", 
-        min=1,
-        max=30,
-        value=15)
+      selectInput(
+        inputId="cut",
+        label="Select Cut of Diamond:", 
+        choices = c("Fair", "Good", "Very Good"),
+        selected = "Good")
         ), 
     
     mainPanel(
-      plotOutput(outputId="plot") 
+      tabsetPanel( 
+        tabPanel("Price vs Carat",
+                 plotOutput(outputId = "scatter")
+        ),
+        tabPanel("Volume by Clarity class",
+                 plotOutput(outputId = "bar")
+        )
       )
-  ) 
+    )
+  )
 )
-  
-server <- function(input, output){ 
-  output$plot <- renderPlot({
-    ggplot(data=diamonds, aes(x=price)) + geom_histogram() + stat_bin(bins=input$bands)
-    })
-} 
 
+server <- function(input, output){ 
+  getDataset <- reactive({
+    if (input$cut=="Fair") {
+      return(diamonds[diamonds$cut=="Fair", ])
+    } else if (input$cut=="Good") {
+      return(diamonds[diamonds$cut=="Good", ])
+    } else {
+      return(diamonds[diamonds$cut=="Very Good", ])
+    }
+  })
+  
+  output$scatter <- renderPlot({
+    ggplot(data=getDataset(), aes(x=price, y=carat)) + geom_point(aes(colour=color)) + scale_color_brewer(palette="Blues")
+  })
+  
+  output$bar <- renderPlot({
+    ggplot(data=getDataset(), aes(x=clarity)) + geom_bar(aes(fill=color)) + scale_fill_brewer(palette="Spectral")
+  })
+}
+      
 shinyApp(ui=ui, server=server)
 
               
