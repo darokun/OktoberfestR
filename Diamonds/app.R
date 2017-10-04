8279cb0cbaecf6495d7114189d1b479cdcbf1863
darokun <daloharodriguez@gmail.com>
2017-10-04 14:57:21 +0200

    write chunk 1

---
+++ Diamonds/app.R
@@ -0,0 +1,8 @@
+#Remember to load & define variables for the global environment, e.g. packages, objects
+library(shiny)
+
+ui <- basicPage()
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
