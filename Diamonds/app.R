29eb1874c21cdce6f01d4e804f0eafd7074cc6e6
darokun <daloharodriguez@gmail.com>
2017-10-04 15:06:01 +0200

    write chunk 4

--- Diamonds/app.R
+++ Diamonds/app.R
@@ -4,7 +4,15 @@ library(shiny)
 ui <- fluidPage(
                 titlePanel("Title"), 
                 sidebarLayout(
-                              sidebarPanel("Sidebar Panel"), 
+                              sidebarPanel("Sidebar Panel",
+                                            sliderInput(
+                                                        inputId="bins",
+                                                        label="Slider Label", 
+                                                        min=1,
+                                                        max=30,
+                                                        value=15
+                                                        )
+                                            ),
                               mainPanel("Main Panel")
                               )
                 )
