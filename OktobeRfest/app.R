4f66954245e9fea23bebd190c7dacf8834ad8d1a
darokun <daloharodriguez@gmail.com>
2017-10-04 14:55:33 +0200

    add *.Rhistory

--- OktobeRfest/app.R
+++ OktobeRfest/app.R
@@ -1,7 +1,25 @@
 library(shiny)
 library(tidyverse)
 
-data <- read.csv("dataOktoberfest.csv")
+rawDataUrl <- "https://www.opengov-muenchen.de/dataset/8d6c8251-7956-4f92-8c96-f79106aab828/resource/e0f664cf-6dd9-4743-bd2b-81a8b18bd1d2/download/oktoberfestgesamt19852016.csv"
+
+# read data
+data <- read.csv(rawDataUrl)
+
+# rename variables DEU-ENG
+data <- rename(data, year = jahr, 
+               duration_days = dauer,
+               visitor_year = besucher_gesamt, 
+               visitor_day = besucher_tag, 
+               beer_price = bier_preis,
+               beer_sold = bier_konsum,
+               chicken_price = hendl_preis,
+               chicken_sold = hendl_konsum)
+
+# unify measures
+data$visitor_year <- data$visitor_year * 1000000
+data$visitor_day <- data$visitor_day * 1000
+data$beer_sold <- data$beer_sold * 100
 
 ui <- fluidPage(
   
@@ -13,57 +31,70 @@ ui <- fluidPage(
     sidebarPanel(
       sliderInput(inputId = "yearInput",
                   label = "Choose a range of years",
-                  min = 1985,
-                  max = 2016,
-                  value = c(1985, 2016),
-                  sep = ""),
+                  value = NULL,
+                  min = NULL,
+                  max = NULL),
       
-      radioButtons(inputId = "variableInput",
+      selectInput(inputId = "variableInput",
                   label = "What do you want to visualize?",
-                  choices = c("Mean visitors per day",
-                              "Visitors per year",
-                              "Beer price",
-                              "Amount of beer sold",
-                              "Chicken price",
-                              "Amount of chicken sold"),
-                  selected = "Beer price")
+                  selected = "")
     ),
   
   
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput(outputId = "distPlot"),
-      br(), br(),
-      tableOutput(outputId = "summaryTable")
+      tableOutput(outputId = "table1")
+      # ,
+      # plotOutput(outputId = "distPlot"),
+      # br(), br(),
+      # tableOutput(outputId = "summaryTable")
     )
   )
 )
 
-server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    filtered <-
-      data %>%
-      filter(year >= input$yearInput[1],
-             year <= input$yearInput[2]) %>%
-      select(
-        # visitor_day == input$variableInput[1],
-             visitor_year == input$variableInput[2],
-             beer_price == input$variableInput[3],
-             beer_sold == input$variableInput[4],
-             chicken_price == input$variableInput[5],
-             chicken_sold == input$variableInput[6]
-             )
-
-    ggplot(filtered) + 
-      geom_bar(aes(input$yearInput, input$variableInput)) +
-      scale_y_continuous(
-        labels = scales::comma, 
-        limits = c(0, 8000000),
-        breaks = seq(0, 8000000, by = 1000000)) +
-      scale_x_continuous(breaks = seq(1985, 2016, by = 1))
-
-    })
+server <- shinyServer(function(input, output, session) {
   
-}
+  updateSliderInput(session, inputId = 'yearInput', label = 'Year',
+                    min = 1985,
+                    max = 2016,
+                    value = c(min(data$year), max(data$year)),
+                    sep = "")
+  updateSelectInput(session, inputId = 'variableInput', label = 'What do you want to visualize?',
+                    choices = names(data), selected = names(data))
+  
+  
+  # data$visitor_day == input$variableInput[1]
+  # data$visitor_year == input$variableInput[2]
+  # data$beer_price == input$variableInput[3]
+  # data$beer_sold == input$variableInput[4]
+  # data$chicken_price == input$variableInput[5]
+  # data$chicken_sold == input$variableInput[6]
+  
+  df_subset <- reactive({
+    a <- tibble(data$year, input$variableInput)
+    # a <- a %>%
+    #       filter(data$year >= input$yearInput[1],
+    #              data$year <= input$yearInput[2])
+    return(a)
+  })
+  
+  output$table1 <- renderTable(df_subset())
+  
+  # output$distPlot <- renderPlot({
+  #   filtered <- df_subset() %>%
+  #     filter(year >= input$yearInput[1],
+  #            year <= input$yearInput[2])
+  # 
+  #   ggplot(filtered) + 
+  #     geom_bar(aes(c(input$yearInput[1], input$yearInput[2]), input$variableInput)) +
+  #     scale_y_continuous(
+  #       labels = scales::comma, 
+  #       limits = c(0, 8000000),
+  #       breaks = seq(0, 8000000, by = 1000000)) +
+  #     scale_x_continuous(breaks = seq(1985, 2016, by = 1))
+  # 
+  #   })
+  
+})
 
 shinyApp(ui = ui, server = server)
